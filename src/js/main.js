2347632c0611f6cea812b930fb7ebb0085615d77
Soniamy <marianna.marzullo@gmail.com>
2023-12-05 14:40:31 +0100

    creazione di for in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 //creazione delle variabili
 let $ul = document.getElementById('fizzbuzzer');
 let msg = '';
+//creazione di for
+for (let i = 1; i < 100; i++) {
+    if (i % 15 == 0) msg = 'fizzbuzz';
+    else if (i % 3 == 0) msg = 'fizz';
+    else if (i % 5 == 0) msg = 'buzz';
+    else msg = '';
+    console.log(i);
+}
