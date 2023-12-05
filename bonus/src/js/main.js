efb1e973561f1fdea2f81ae72060bad9e6b42a18
Soniamy <marianna.marzullo@gmail.com>
2023-12-05 16:40:50 +0100

    correzione bonus

--- bonus/src/js/main.js
+++ bonus/src/js/main.js
@@ -1,5 +1,5 @@
 //creazione delle variabili
-let $ul = document.getElementById('fizzbuzzer');
+let ul = document.getElementById('fizzbuzzer');
 let msg = '';
 //creazione di for
 for (let i = 1; i <= 100; i++) {
@@ -7,10 +7,10 @@ for (let i = 1; i <= 100; i++) {
     else if (i % 3 == 0) msg = 'fizz';
     else if (i % 5 == 0) msg = 'buzz';
     else msg = i;
-    console.log(i);
+    console.log(msg);
     //creazione della variabile li
     let li = document.createElement('li');
     li.append(msg);
     li.setAttribute('class', msg);
-    $ul.append(li);
+    ul.append(li);
 }
