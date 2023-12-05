5cd802980857fcb26132d8307b1c3fe237c9873b
Soniamy <marianna.marzullo@gmail.com>
2023-12-05 16:41:18 +0100

    ccorrrezione js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ for (let i = 1; i < 100; i++) {
     else if (i % 3 == 0) msg = 'fizz';
     else if (i % 5 == 0) msg = 'buzz';
     else msg = '';
-    console.log(i);
+    console.log(msg);
     //creazione della variabile li
     let li = document.createElement('li');
     li.append(i + ' = ' + msg);
